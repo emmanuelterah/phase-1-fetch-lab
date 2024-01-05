cf82e2a040e430b09de8bb1e5e68d12899bdbd7b
emmanuelterah <emmanuelterah98@gmail.com>
2024-01-05 12:30:12 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    const bookTitles = data.map(book => ({ name: book.name }));
+    renderBooks(bookTitles);
+  })
+  .catch(error => {
+    console.error('There was a problem fetching the books:', error);
+  });
   
 }
 
